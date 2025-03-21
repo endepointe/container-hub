00bfa7b299b9ab681e2cc0455dd5db8d373e6b6d
endepointe <endepointe@gmail.com>
2025-03-21 08:07:45 -0700

    sorted raw conf and added script

--- sandbox-service-a/ftp/readconf.py
+++ sandbox-service-a/ftp/readconf.py
@@ -8,7 +8,7 @@ either a default, or otherwise, paranoia can be satisfied.
 Remember to has the conf file and monitor it for any changes skid.
 '''
 
-def parse_vsftpd_html_interactive(html_file, of):
+def parse_vsftpd_html(html_file, of):
 
     with open(html_file, 'r', encoding='utf-8') as f:
         html_content = f.read()
@@ -39,7 +39,14 @@ def parse_vsftpd_html_interactive(html_file, of):
                 continue
 
 if __name__ == "__main__":
+    #of = open("default.vsftpd.conf", "w")
+    #parse_vsftpd_html('vsftpd_conf.html',of)
+    #of.close()
+    unsorted = "Python offers two primary ways to sort lists"
     unsorted = unsorted.split(' ')
     for item in unsorted:
         print(item)
+    #with open("default.vsftpd.conf","r") as uf:
+    #    lines = sf.read()
+    #sorted_file = 
     print("Now c134n^7h3f113")
